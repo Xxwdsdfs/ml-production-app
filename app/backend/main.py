4edcb24f2929b2122522d188549f921ccd77a449
Axel BENOIST <axelbenoist@orange.fr>
2024-12-29 20:14:47 +0100

    Test Ml backend1

--- app/backend/main.py
+++ app/backend/main.py
@@ -3,19 +3,19 @@ from flask_cors import CORS  # Import Flask-CORS
 import joblib
 import pandas as pd
 import os
+import mlflow
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 CORS(app)  # Autorise toutes les origines par défaut
 
-# Obtenir le chemin du répertoire courant du fichier (main.py)
-current_dir = os.path.dirname(__file__)
+# Définir l'URI pour MLFlow
+mlflow.set_tracking_uri("http://localhost:5000")  # URI pour accéder à MLFlow UI
+model_uri = "models:/Model_Finalv2/1"  # Remplace 'model_final' par ton modèle enregistré
 
-# Construire un chemin relatif pour le modèle
-model_path = os.path.join(current_dir, "../../ml/model_registry/model.joblib")
-
-# Charger le modèle
-model, encoder = joblib.load(model_path)
-print(f"Modèle chargé depuis : {model_path}")
+# Charger le modèle depuis MLFlow
+model = mlflow.sklearn.load_model(model_uri)
+print(f"Modèle chargé depuis MLFlow : {model_uri}")
 
 # Route d'accueil (pour éviter l'erreur 404)
 @app.route('/', methods=['GET'])
@@ -34,6 +34,9 @@ def predict():
 
     exemple = pd.DataFrame([[total_joueur, carte_croupier]], columns=['Total_Joueur', 'Carte_Croupier'])
     prediction = model.predict(exemple)
+    encoder = LabelEncoder()
+    actions = ['tirer', 'rester']  # Possible actions (tirer/rester)
+    encoder.fit(actions)  # On entraîne l'encoder sur ces valeurs
     action = encoder.inverse_transform(prediction)
 
     return jsonify({"Total_Joueur": total_joueur, "Carte_Croupier": carte_croupier, "Action": action[0]})
